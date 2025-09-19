64f07ddd1f564e2d3febce8c142a11e49f59de36
Robin <lxs5171@mavs.uta.edu>
2025-09-18 21:29:03 -0500

    save

--- Monitoring/kubernetes-full-monitoring/monitoring-install.sh
+++ Monitoring/kubernetes-full-monitoring/monitoring-install.sh
@@ -1,43 +1,244 @@
 # Secure .kube
+microservice_namespace="ms"
+head_node_label="node-role=worker"
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        --ns=*)
+            microservice_namespace="${1#*=}"
+            shift
+            ;;
+        --head-node-label=*)
+            head_node_label="${1#*=}"
+            shift
+            ;;
+        *)
+            shift
+            ;;
+    esac
+done
+
+
+
+if [[ -z $(kubectl get namespace ${microservice_namespace}) ]]; then
+    echo "Namespace ${microservice_namespace} does not exist, creating it"
+    kubectl create namespace ${microservice_namespace}
+fi
+
+head_node_label_key=$(echo $head_node_label | cut -d'=' -f1)  
+head_node_label_value=$(echo $head_node_label | cut -d'=' -f2)
+echo "Head node label key: ${head_node_label_key}"
+echo "Head node label value: ${head_node_label_value}"
+
+# #########################################################################################################################
+# Uninstall if already installed
+# #########################################################################################################################
+if [[ -n $(helm list -n monitoring) ]]; then
+    echo "Uninstalling prometheus, istio-base, istiod, istio-ingressgateway, jaeger, kiali"
+    helm uninstall prometheus -n monitoring
+    helm uninstall istio-base -n istio-system
+    helm uninstall istiod -n istio-system
+    helm uninstall istio-ingressgateway -n istio-system
+    helm uninstall jaeger -n jaeger
+    helm uninstall kiali -n kiali
+    echo "Deleting namespace monitoring"
+    kubectl delete namespace monitoring
+    echo "Deleting namespace istio-system"
+    kubectl delete namespace istio-system
+    echo "Existing monitoring components deleted"
+fi
+
 chmod go-r -R ~/.kube/
 
-# Prometherus
+# #########################################################################################################################
+# Prometheus
+# #########################################################################################################################
 kubectl create namespace monitoring
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
-helm install prometheus prometheus-community/kube-prometheus-stack -n monitoring
+
+
+cat <<EOF > prometheus-values.yaml
+# values.yaml
+
+# Global defaults for affinity
+global:
+  podAffinityPreset: ""
+  podAntiAffinityPreset: ""
+  nodeAffinityPreset:
+    type: hard
+    key: ${head_node_label_key}
+    values:
+      - ${head_node_label_value}
+
+# Or more fine-grained (per component):
+prometheus:
+  prometheusSpec:
+    affinity:
+      nodeAffinity:
+        requiredDuringSchedulingIgnoredDuringExecution:
+          nodeSelectorTerms:
+          - matchExpressions:
+            - key: ${head_node_label_key}
+              operator: In
+              values:
+              - ${head_node_label_value}
+
+alertmanager:
+  alertmanagerSpec:
+    affinity:
+      nodeAffinity:
+        requiredDuringSchedulingIgnoredDuringExecution:
+          nodeSelectorTerms:
+          - matchExpressions:
+            - key: ${head_node_label_key}
+              operator: In
+              values:
+              - ${head_node_label_value}
+
+grafana:
+  affinity:
+    nodeAffinity:
+      requiredDuringSchedulingIgnoredDuringExecution:
+        nodeSelectorTerms:
+        - matchExpressions:
+          - key: ${head_node_label_key}
+            operator: In
+            values:
+            - ${head_node_label_value}
+EOF
+
+
+helm install prometheus prometheus-community/kube-prometheus-stack -n monitoring -f prometheus-values.yaml
 
 # Prometheus (30000) and Grafana (30001) NodePort Services
 kubectl apply -f prometheus-nodeport.yaml -n monitoring
 kubectl apply -f grafana-nodeport.yaml -n monitoring
 
+# #########################################################################################################################
 # Istio
+# #########################################################################################################################
 helm repo add istio https://istio-release.storage.googleapis.com/charts
 helm repo update
 kubectl create namespace istio-system
-helm install istio-base istio/base -n istio-system
-helm install istiod istio/istiod -n istio-system --set global.proxy.tracer="zipkin" --wait
-helm install istio-ingressgateway istio/gateway -n istio-system
-kubectl label namespace default istio-injection=enabled
+
+cat <<EOF > istio-values.yaml
+
+pilot:
+  nodeSelector:
+    ${head_node_label_key}: ${head_node_label_value}
+  # Or full affinity if you want stricter control
+  affinity:
+    nodeAffinity:
+      requiredDuringSchedulingIgnoredDuringExecution:
+        nodeSelectorTerms:
+        - matchExpressions:
+          - key: ${head_node_label_key}
+            operator: In
+            values:
+            - ${head_node_label_value}
+
+# Gateway (Ingress/Egress)
+gateways:
+  istio-ingressgateway:
+    nodeSelector:
+      ${head_node_label_key}: ${head_node_label_value}
+    affinity:
+      nodeAffinity:
+        requiredDuringSchedulingIgnoredDuringExecution:
+          nodeSelectorTerms:
+          - matchExpressions:
+            - key: ${head_node_label_key}
+              operator: In
+              values:
+              - ${head_node_label_value}
+EOF
+
+helm install istio-base istio/base -n istio-system -f istio-values.yaml
+
+cat <<EOF > istiod-scheduling.yaml
+# istiod-scheduling.yaml
+pilot:
+  nodeSelector:
+    ${head_node_label_key}: ${head_node_label_value}
+  affinity:
+    nodeAffinity:
+      requiredDuringSchedulingIgnoredDuringExecution:
+        nodeSelectorTerms:
+        - matchExpressions:
+          - key: ${head_node_label_key}
+            operator: In
+            values:
+              - ${head_node_label_value}
+EOF
+
+helm install istiod istio/istiod -n istio-system --set global.proxy.tracer="zipkin" --wait -f istiod-scheduling.yaml
+
+
+cat <<EOF > istio-ingressgateway-scheduling.yaml
+# ingress-scheduling.yaml
+gateways:
+  istio-ingressgateway:
+    nodeSelector:
+      ${head_node_label_key}: ${head_node_label_value}
+    affinity:
+      nodeAffinity:
+        requiredDuringSchedulingIgnoredDuringExecution:
+          nodeSelectorTerms:
+          - matchExpressions:
+            - key: ${head_node_label_key}
+              operator: In
+              values:
+                - ${head_node_label_value}
+
+EOF
+helm install istio-ingressgateway istio/gateway -n istio-system -f istio-ingressgateway-scheduling.yaml
+kubectl label namespace ${microservice_namespace} istio-injection=enabled
 
 # Istio - Prometeus integration
 kubectl apply -f istio-prometheus-operator.yaml
 
-# Jarger
+# #########################################################################################################################
+# Jaeger
+# #########################################################################################################################
+sed -i "s/node-role/${head_node_label_key}/g" -i "s/worker/${head_node_label_value}/g" jaeger.yaml
 kubectl apply -f jaeger.yaml
 
 # Jaeger NodePort Service (30002)
 kubectl apply -f jaeger-nodeport.yaml
 
-#Kiali
+# #########################################################################################################################
+# Kiali
+# #########################################################################################################################
 helm repo add kiali https://kiali.org/helm-charts
 helm repo update
+
+cat <<EOF > kiali-scheduling.yaml
+
+deployment:
+  nodeSelector:
+    ${head_node_label_key}: ${head_node_label_value}
+
+  affinity:
+    nodeAffinity:
+      requiredDuringSchedulingIgnoredDuringExecution:
+        nodeSelectorTerms:
+        - matchExpressions:
+          - key: ${head_node_label_key}
+            operator: In
+            values:
+              - ${head_node_label_value}
+EOF
+
 helm install \
   -n istio-system \
   -f kiali-values.yaml \
+  -f kiali-scheduling.yaml \
   kiali-server \
   kiali/kiali-server
 
 #Kiali NodePort Service (30003)
 kubectl apply -f kiali-nodeport.yaml
+
+# #########################################################################################################################
+echo "Monitoring installation completed"
 
