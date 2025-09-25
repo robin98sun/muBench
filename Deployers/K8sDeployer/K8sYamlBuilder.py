b097f38aaa7c39ee4a38bd2f1a1cb57b6d2bca39
Robin <lxs5171@mavs.uta.edu>
2025-09-25 00:21:41 -0500

    save

--- Deployers/K8sDeployer/K8sYamlBuilder.py
+++ Deployers/K8sDeployer/K8sYamlBuilder.py
@@ -114,10 +114,10 @@ def create_deployment_service_yaml_files(workmodel, k8s_parameters, nfs, output_
                 host_files_mount_paths = []
                 host_files_volumes = []
                 for host_file in workmodel[service]["host_files"]:
-                    host_files_mount_paths.append({"name": host_file['name'], "mountPath": f"/app/{host_file['name']}", "readOnly": True})
-                    host_files_volumes.append({"name": host_file['name'], "hostPath": host_file['path'], "type": "File"})
-                f = f.replace("{{HOST_FILES_MOUNT_PATHS}}", str(yaml.dump(host_files_mount_paths)).replace('\n','\n        ').rstrip())
-                f = f.replace("{{HOST_FILES_VOLUMES}}", str(yaml.dump(host_files_volumes)).replace('\n','\n        ').rstrip())
+                    host_files_mount_paths.append(f"- name: {host_file['name']}\n            mountPath: /app/{host_file['name']}\n            readOnly: true")
+                    host_files_volumes.append(f"- name: {host_file['name']}\n            hostPath: {host_file['path']}\n            type: File")
+                f = f.replace("{{HOST_FILES_MOUNT_PATHS}}", "\n            ".join(host_files_mount_paths))
+                f = f.replace("{{HOST_FILES_VOLUMES}}", "\n        ".join(host_files_volumes))
             else:
                 f = f.replace("{{HOST_FILES_MOUNT_PATHS}}", "")
                 f = f.replace("{{HOST_FILES_VOLUMES}}", "")
