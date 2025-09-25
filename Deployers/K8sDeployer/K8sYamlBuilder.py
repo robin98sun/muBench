bc7c27a70dfcb51f9fb04200f5317831cbf462f4
Robin <lxs5171@mavs.uta.edu>
2025-09-25 13:56:34 -0500

    save

--- Deployers/K8sDeployer/K8sYamlBuilder.py
+++ Deployers/K8sDeployer/K8sYamlBuilder.py
@@ -67,7 +67,7 @@ def create_deployment_service_yaml_files(workmodel, k8s_parameters, nfs, output_
                 f = f.replace("{{SIDECAR}}", "".rstrip())
             
             if "ms-replica" in k8s_parameters.keys():
-                f = f.replace("{{MS_REPLICAS}}", str(k8s_parameters["ms-replica"]))
+                f = f.replace("{{REPLICAS}}", str(k8s_parameters["ms-replica"]))
             elif "replicas" in workmodel[service].keys():
                 f = f.replace("{{REPLICAS}}", str(workmodel[service]["replicas"]))
             else:
