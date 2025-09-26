d7323e4c85a2066575da7b48b1c323be2849b4d7
Robin <lxs5171@mavs.uta.edu>
2025-09-26 17:06:51 -0500

    save

--- Deployers/K8sDeployer/K8sYamlBuilder.py
+++ Deployers/K8sDeployer/K8sYamlBuilder.py
@@ -75,7 +75,11 @@ def create_deployment_service_yaml_files(workmodel, k8s_parameters, nfs, output_
             
             if "worker-node-affinity" in k8s_parameters.keys():
                 NODE_AFFINITY_TEMPLATE_TO_ADD = NODE_AFFINITY_TEMPLATE.copy()
-                NODE_AFFINITY_TEMPLATE_TO_ADD['affinity']['nodeAffinity']['requiredDuringSchedulingIgnoredDuringExecution']['nodeSelectorTerms'][0]['matchExpressions'][0].update({"values" : k8s_parameters["worker-node-affinity"]["values"]})
+                # Ensure values is always a list
+                worker_affinity_values = k8s_parameters["worker-node-affinity"]["values"]
+                if isinstance(worker_affinity_values, str):
+                    worker_affinity_values = [worker_affinity_values]
+                NODE_AFFINITY_TEMPLATE_TO_ADD['affinity']['nodeAffinity']['requiredDuringSchedulingIgnoredDuringExecution']['nodeSelectorTerms'][0]['matchExpressions'][0].update({"values" : worker_affinity_values})
                 NODE_AFFINITY_TEMPLATE_TO_ADD['affinity']['nodeAffinity']['requiredDuringSchedulingIgnoredDuringExecution']['nodeSelectorTerms'][0]['matchExpressions'][0].update({"key" : k8s_parameters["worker-node-affinity"]["key"]})
                 f = f.replace("{{NODE_AFFINITY}}", str(yaml.dump(NODE_AFFINITY_TEMPLATE_TO_ADD)).rstrip().replace('\n','\n      '))
             elif "node_affinity" in workmodel[service].keys():
